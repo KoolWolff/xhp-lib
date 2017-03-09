bc4c11a17e2d3b68102963feb73a292a75b6106c
Fred Emmott <fred@fredemmott.co.uk>
2017-03-09 08:13:39 -0800

    Make .travis.sh executable, fetch composer outside docker

--- .travis.sh
+++ .travis.sh
@@ -2,10 +2,6 @@
 set -e
 set -x
 cd /var/source
-apt-get update -y
-apt-get install -y curl
-curl -sS https://getcomposer.org/installer | \
-  hhvm --php -- --install-dir=/opt/composer
-hhvm -d hhvm.jit=0 /opt/composer/composer.phar install
+hhvm -d hhvm.jit=0 composer.phar install
 hh_client
 hhvm -d hhvm.jit=0 vendor/bin/phpunit
